b1ca39cc85104fdaa6e28c16a4637889ed0dc974
vincent <vincentnys@gmail.com>
2013-08-14 14:37:40 +0200

    Made changes in obvious places.

--- python_env/src/sight_reading/compose.py
+++ python_env/src/sight_reading/compose.py
@@ -10,7 +10,7 @@ from pykov import maximum_likelihood_probabilities
 from sight_reading.synthesis.apopcaleaps_interface import compose
 
 
-def _get_dependencies(mode):
+def _get_dependencies(mode, key_mode):
     my_dir = os.path.dirname(os.path.realpath(__file__))
     runtime_dir = my_dir.replace('src','runtime')
     parameter_path = os.path.join(runtime_dir, 'parameters.ini')
@@ -20,7 +20,7 @@ def _get_dependencies(mode):
     music_path = config.get('APOPCALEAPS', 'music_folder')
     data_path = config.get('Analysis', 'data_root')
 
-    with open(os.path.join(data_path, 'pickled_r_percentiles')) as fh:
+    with open(os.path.join(data_path, 'pickled_r_percentiles'+key_mode)) as fh:
         r_percentiles = pickle.load(fh)
 
     with open(os.path.join(data_path, 'pickled_rhythm')) as fh:
@@ -29,7 +29,7 @@ def _get_dependencies(mode):
         r_chain = P
 
     if mode == 'Temperley':
-        m_percentiles_path = os.path.join(data_path, 'pickled_m_percentiles_temperley')
+        m_percentiles_path = os.path.join(data_path, 'pickled_m_percentiles_temperley'+key_mode)
         m_chain = None
     elif mode == 'Relative':
         m_chain_path = os.path.join(data_path, 'pickled_melody_relative')
@@ -63,8 +63,10 @@ def multi_compose(mode='Relative'):
     music_path, r_chain, r_percentiles, m_chain, m_percentiles= _get_dependencies(mode)
     r_sections = range(0, len(r_percentiles))
     m_sections = range(0, len(m_percentiles))
+    key_modes = ['major', 'minor']
     results = []
-    for (section_r, section_m) in product(r_sections, m_sections):
+    all_combinations = product(key_modes, product(r_sections, m_sections))
+    for (section_r, section_m, key_mode) in all_combinations:
         start_time = clock()
         compose(music_path,
                 r_chain, r_percentiles, section_r,
@@ -75,10 +77,10 @@ def multi_compose(mode='Relative'):
     return results
 
 
-def make_composition(mode, r_section, m_section):
-    music_path, r_chain, r_percentiles, m_chain, m_percentiles, = _get_dependencies(mode)
+def make_composition(mode, r_section, m_section, key_mode):
+    music_path, r_chain, r_percentiles, m_chain, m_percentiles, = _get_dependencies(mode, key_mode)
     compose(music_path, r_chain, r_percentiles, r_section,
-            m_chain, m_percentiles, m_section, mode
+            m_chain, m_percentiles, m_section, mode, key_mode
             )
 
 
@@ -90,6 +92,7 @@ if __name__ == '__main__':
     arg_parser.add_argument('mode', help=explanation)
     arg_parser.add_argument('percentile_rhythm', help='0-indexed percentile')
     arg_parser.add_argument('percentile_melody', help='0-indexed percentile')
+    arg_parser.add_argument('key_mode', help='major or minor')
     args = arg_parser.parse_args()
     mode = args.mode
     if mode:
@@ -98,5 +101,6 @@ if __name__ == '__main__':
         mode = 'Relative'
     percentile_rhythm = int(args.percentile_rhythm)
     percentile_melody = int(args.percentile_melody)
+    key_mode = args.key_mode
 
-    make_composition(mode, percentile_rhythm, percentile_melody)  # 0-indexed percentiles
+    make_composition(mode, percentile_rhythm, percentile_melody, key_mode)  # 0-indexed percentiles
